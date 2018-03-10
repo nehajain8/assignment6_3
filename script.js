7286e7872f0c15490382232ceb19f26b320d9f07
nehajain8 <35501045+nehajain8@users.noreply.github.com>
2018-03-10 15:33:35 +0530

    Assignment 6-3

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Operators and expressions */ 
+var temperatureInCelsius = 40;
+var celsiusToFahrenheit = (temperatureInCelsius*9)/5 + 32;
+var temperatureInFahrenheit = 104;
+var fahrenheitToCelsius = ((temperatureInFahrenheit-32)*5)/9;
+console.log("Temperature of " + temperatureInCelsius + " celsius, will be " + celsiusToFahrenheit + " fahrenheit");
+console.log("Temperature of " + temperatureInFahrenheit + " fahrenheit, will be " + celsiusToFahrenheit + " celsius");
+
+
+
